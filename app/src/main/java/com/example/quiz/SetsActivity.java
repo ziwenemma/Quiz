ce83e673e05a625e5ad4e386a5c51db9093479ae
Sandeep <seemu34@gmail.com>
2021-01-10 16:36:30 -0500

    fetch sets from firestore and add progress bar

--- app/src/main/java/com/example/quiz/SetsActivity.java
+++ app/src/main/java/com/example/quiz/SetsActivity.java
@@ -4,18 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.GridView;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import static com.example.quiz.SplashActivity.catList;
 
 
 public class SetsActivity extends AppCompatActivity {
 
     private GridView sets_grid;
-
-
+    private FirebaseFirestore firestore;
+    private int category_id;
+    private Dialog loadingDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,17 +36,61 @@ public class SetsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         String title = getIntent().getStringExtra("CATEGORY");
+        category_id = getIntent().getIntExtra("CATEGORY_ID",1);
+
         getSupportActionBar().setTitle(title);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         sets_grid = findViewById(R.id.sets_gridview);
 
-        SetsAdapter adapter = new SetsAdapter(10);
+        loadingDialog = new Dialog(SetsActivity.this);
+        loadingDialog.setContentView(R.layout.loading_progressbar);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setBackgroundDrawableResource(R.drawable.progress_background);
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        loadingDialog.show();
 
-        sets_grid.setAdapter(adapter);
+        firestore = FirebaseFirestore.getInstance();
+
+        loadSets();
+
+
 
     }
+
+    public void loadSets(){
+        firestore.collection("QUIZ").document("CAT" + String.valueOf(category_id))
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+
+                    if (doc.exists()) {
+                        long sets = (long) doc.get("SETS");
+
+
+                        SetsAdapter adapter = new SetsAdapter((int) sets);
+
+                        sets_grid.setAdapter(adapter);
+
+
+                    } else {
+                        Toast.makeText(com.example.quiz.SetsActivity.this, "No Category Document Exists!", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                } else {
+
+                    Toast.makeText(com.example.quiz.SetsActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                loadingDialog.cancel();
+            }
+
+            });
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
