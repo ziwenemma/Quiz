e09b2998c93c7c8df81254be82c0219240ef4a1f
sandeep-thind <61361012+sandeep-thind@users.noreply.github.com>
2021-01-15 13:01:55 -0500

    Update RegisterPage.java
    fix the error

--- app/src/main/java/com/example/quiz/RegisterPage.java
+++ app/src/main/java/com/example/quiz/RegisterPage.java
@@ -60,7 +60,124 @@ public class RegisterPage extends AppCompatActivity {
 
         signupButton.setOnClickListener(new View.OnClickListener() {
 
+                                            @Override
+                                            public void onClick(View v) {
+                                                final String email = mEmail.getText().toString().trim();
+                                                String password = mPassword.getText().toString().trim();
+                                                final String fullName = username.getText().toString();
+                                                String comfirmPass = mConfirm.getText().toString().trim();
 
+
+                                                if (TextUtils.isEmpty(email)) {
+                                                    mEmail.setError("Email is Required.");
+                                                    return;
+                                                }
+                                                if (TextUtils.isEmpty(password)) {
+                                                    mPassword.setError("password is required.");
+                                                    return;
+                                                }
+                                                if (password.length() < 6) {
+                                                    mPassword.setError("password mush be more than 6 characters");
+                                                    return;
+                                                }
+                                                if (password.compareTo(comfirmPass) != 0) {
+                                                    mConfirm.setError("password are not same at two times");
+                                                    return;
+                                                }
+
+
+                                                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Toast.makeText(RegisterPage.this, "User Created", Toast.LENGTH_LONG).show();
+                                                            FirebaseUser fuser = fAuth.getCurrentUser();
+
+                                                            fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                                                                @Override
+                                                                public void onSuccess(Void aVoid) {
+
+                                                                    DbQuery.loadCategories(new MyCompleteListener() {
+                                                                        @Override
+                                                                        public void onSuccess() {
+                                                                            Intent intent = new Intent(RegisterPage.this,MainPage.class);
+                                                                            startActivity(intent);
+                                                                            finish();
+                                                                        }
+
+                                                                        @Override
+                                                                        public void onFailure() {
+                                                                            Toast.makeText(RegisterPage.this, "Error!" ,Toast.LENGTH_LONG).show();
+
+                                                                        }
+                                                                    });
+
+
+
+                                                                    Toast.makeText(RegisterPage.this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show();
+                                                                }
+                                                            }).addOnFailureListener(new OnFailureListener() {
+
+                                                                @Override
+                                                                public void onFailure(@NonNull Exception e) {
+                                                                    Log.d(TAG, "onFailure: Email not sent " + e.getMessage());
+                                                                }
+                                                            });
+
+
+                                                            DbQuery.createUserData(email, fullName, new MyCompleteListener() {
+
+                                                                @Override
+                                                                public void onSuccess() {
+
+                                                                }
+
+                                                                @Override
+                                                                public void onFailure() {
+
+                                                                }
+                                                            });
+
+
+                                                            userID = fAuth.getCurrentUser().getUid();
+                                                            DocumentReference documentReference = fStore.collection("users").document(userID);
+
+                                                            Map<String, Object> user = new HashMap<>();
+                                                            user.put("NAME", fullName);
+                                                            user.put("EMAIL_ID", email);
+                                                            user.put("password", comfirmPass);
+                                                            user.put("TOTAL_SCORE",0);
+
+
+                                                            DocumentReference userDo = fStore.collection("USERS").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                                            WriteBatch batch =fStore.batch();
+
+
+                                                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                                                                @Override
+                                                                public void onSuccess(Void aVoid) {
+                                                                    Log.d(TAG, "onSuccess: user Profile is created for " + userID);
+                                                                }
+                                                            }).addOnFailureListener(new OnFailureListener() {
+                                                                @Override
+                                                                public void onFailure(@NonNull Exception e) {
+                                                                    Log.d(TAG, "onFailure: " + e.toString());
+                                                                }
+                                                            });
+                                                            startActivity(new Intent(getApplicationContext(), MainPage.class));
+
+                                                        } else {
+                                                            Toast.makeText(RegisterPage.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                                        }
+                                                    }
+                                                });
+
+
+                                            }
+                                        });
 
 
 
